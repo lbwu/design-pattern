03e84636436f1d7bce52019fd39e40318ce29e3c
吴林波 <wulinbo@digihealth.net>
2019-11-02 22:54:20 +0800

    迭代协议

--- demo04.js
+++ demo04.js
@@ -33,3 +33,26 @@ console.log(itor.next())
 console.log(itor.next())
 console.log(itor.next())
 console.log(itor.next())
+
+
+// 可迭代协议
+// 为了变成可迭代对象， 一个对象必须实现 @@iterator 方法, 
+// 意思是这个对象（或者它原型链 prototype chain 上的某个对象）
+//必须有一个名字是 Symbol.iterator 的属性:
+// [Symbol.iterator]	返回一个对象的无参函数，被返回对象符合迭代器协议。
+
+// 迭代器协议
+// 它实现了一个 next() 的方法并且拥有以下含义：
+// next 方法必须要返回一一个对象，该对象有两个必要的属性： done和value
+
+var someString = 'hello world';
+var someStringIterator = someString[Symbol.iterator]();
+
+console.log(someStringIterator.next())
+console.log(someStringIterator.next())
+console.log(someStringIterator.next())
+console.log(someStringIterator.next())
+
+// 一些内置的语法结构，比如spread operator （[...value]），内部也使用了同样的迭代协议
+var somewords = 'hello world';
+console.log([...somewords]) // ["h", "e", "l", "l", "o"]
